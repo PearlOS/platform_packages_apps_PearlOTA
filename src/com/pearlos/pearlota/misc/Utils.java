a6f0ea5ad005a460bd9fa7ca0b7f5c0e67177cb8
Aryan <arykedare@gmail.com>
2019-05-20 06:55:24 +0530

    OTA: Fix Derp

--- src/com/pearlos/pearlota/misc/Utils.java
+++ src/com/pearlos/pearlota/misc/Utils.java
@@ -149,7 +149,6 @@ public class Utils {
     }
 
     public static String getServerURL(Context context) {
-        String incrementalVersion = SystemProperties.get(Constants.PROP_BUILD_VERSION_INCREMENTAL);
         String device = SystemProperties.get(Constants.PROP_NEXT_DEVICE,
                 SystemProperties.get(Constants.PROP_DEVICE));
         String type = SystemProperties.get(Constants.PROP_RELEASE_TYPE).toLowerCase(Locale.ROOT);
